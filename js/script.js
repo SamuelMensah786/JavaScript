8806bee8b08c7cccc6d960ae87d72b96c3d45f05
SamuelMensah786 <bentleyk009@gmail.com>
2022-03-11 11:24:19 -0500

    JavaScript Day 3

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let firstname = "Samuel";
+let lastname = "Mensah";
+let age = 130;
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+
+console.log(
+  "My name is " + firstname + " " + lastname + ". I am " + age + " Years Old."
+);
+
+console.log(`My name is ${firstname} ${lastname}. I am ${age} years old.`);
+
+let fruit = ["mango", "orange", "apple"];
+console.log(fruit);
+console.log(fruit.length);
+console.log(fruit[0]);
+console.log(fruit.push("Watermelon"));
+console.log(fruit);
+console.log(fruit.pop());
+console.log(fruit);
+console.log(fruit.unshift("apple"));
+console.log(fruit);
+console.log(fruit.shift());
+console.log(fruit);
+console.log(fruit.includes("apple"));
+console.log(fruit.includes("Banana"));
+console.log(fruit.join("-"));
